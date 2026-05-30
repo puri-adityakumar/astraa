292b8437d4962e2626fa15095a11dbedc72ae1a5
puri-adityakumar <kumar.adityapuri@gmail.com>
2026-05-30 12:00:40 +0530

    feat(base64): expose public surface via barrel

--- lib/base64/index.ts
+++ lib/base64/index.ts
@@ -1,1 +1,16 @@
 export * from "./types";
+export {
+  encodeText,
+  encodeBytes,
+  decodeToText,
+  decodeToBytes,
+} from "./codec";
+export { validateBase64 } from "./validate";
+export type { ValidationResult, ValidateOptions } from "./validate";
+export {
+  readFileAsBytes,
+  sniffImageMime,
+  FileTooLargeError,
+} from "./file";
+export type { ImageMime } from "./file";
+export { downloadAsFile } from "./download";
